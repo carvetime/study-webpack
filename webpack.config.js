4a11a601fec66a8626a304bff9b9fb6158220a94
gaowenjie7 <yidong-ios16>
2019-05-31 15:15:01 +0800

    modify example 01

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './examples/01/src/index.js',
+    entry: './examples/01/code/src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'examples/01/dist')
